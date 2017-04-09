703da319ceef63d5715acc64b9536c33171f6940
akshayheda <akshayheda@gmail.com>
2017-04-09 01:08:15 -0700

    finished http requests for matches table

--- BasedVex/MatchesViewController.swift
+++ BasedVex/MatchesViewController.swift
@@ -13,6 +13,8 @@ import SwiftyJSON
 
 class MatchesViewController: UIViewController{
     
+    var matchNumber = [String]()
+    
      var red1 = [String]()
      var red2 = [String]()
      var red3 = [String]()
@@ -22,6 +24,11 @@ class MatchesViewController: UIViewController{
     var blue3 = [String]()
     var blueSit = [String]()
     
+    var redScore = [String]()
+    var blueScore = [String]()
+    
+    
+    
     
     
     
@@ -30,6 +37,9 @@ class MatchesViewController: UIViewController{
         super.viewDidLoad()
         Alamofire.request("https://api.vexdb.io/v1/get_matches?sku=RE-VRC-16-5372").responseJSON { response in
             let json = JSON(response.result.value)
+            
+            let matchNumber = json["result"].arrayValue.map({$0["matchnum"].stringValue})
+            
             let red1 = json["result"].arrayValue.map({$0["red1"].stringValue})
             let red12 = json["result"].arrayValue.map({$0["red2"].stringValue})
             let red3 = json["result"].arrayValue.map({$0["red3"].stringValue})
@@ -38,6 +48,9 @@ class MatchesViewController: UIViewController{
             let blue2 = json["result"].arrayValue.map({$0["blue2"].stringValue})
             let blue3 = json["result"].arrayValue.map({$0["blue3"].stringValue})
             let blueSit = json["result"].arrayValue.map({$0["bluesit"].stringValue})
+            
+            let redScore = json["result"].arrayValue.map({$0["redscore"].stringValue})
+            let blueScore = json["result"].arrayValue.map({$0["bluescore"].stringValue})
             
                     }
         // Do any additional setup after loading the view.
